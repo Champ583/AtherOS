5369781ae3782a7b1f4782970d62b4b20cf8d61a
Champ583 <champ20140214@gmail.com>
2024-10-16 16:52:33 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 打开窗口
+function openWindow(windowId) {
+  document.getElementById(windowId).style.display = 'block';
+}
+
+// 关闭窗口
+function closeWindow(windowId) {
+  document.getElementById(windowId).style.display = 'none';
+}
